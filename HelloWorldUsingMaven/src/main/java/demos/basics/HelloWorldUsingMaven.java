59bc1441c24985244f395ab1fefebebdd91437f5
ajaybhosekar <ajaybhosekar@gmail.com>
2025-11-14 01:25:09 +0530

    HelloWorldUsingMaven added

---
+++ HelloWorldUsingMaven/src/main/java/demos/basics/HelloWorldUsingMaven.java
@@ -0,0 +1,11 @@
+package demos.basics;
+
+public class HelloWorldUsingMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World using maven");
+
+	}
+
+}
